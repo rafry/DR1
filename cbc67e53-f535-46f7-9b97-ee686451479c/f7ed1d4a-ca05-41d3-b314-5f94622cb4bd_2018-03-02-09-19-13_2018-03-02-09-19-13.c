62fdff45a05496ede4c239b848f4988c56da53d0
rafry <rafry@USARRRAFRY0.us.deloitte.com>
2018-03-02 17:03:47 -0500

    Commited on..  <d>2018/03/02 17:03:47

---
+++ cbc67e53-f535-46f7-9b97-ee686451479c/f7ed1d4a-ca05-41d3-b314-5f94622cb4bd_2018-03-02-09-19-13_2018-03-02-09-19-13.c
@@ -0,0 +1,8 @@
+{
+  "action": "add",
+  "comment": "Shared Gene Associations among Liver Carcinoma, Breast Carcinoma, Schizophrenia, and Alzheimer\u0027s Disease",
+  "createdTimeStamp": "2018-03-02-09-19-13",
+  "engine": "DR1",
+  "id": "f7ed1d4a-ca05-41d3-b314-5f94622cb4bd_2018-03-02-09-19-13",
+  "insightId": "cbc67e53-f535-46f7-9b97-ee686451479c"
+}
